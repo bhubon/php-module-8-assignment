db80756d8a9e0ac981bc52a238e3ea0612b0e90b
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-11-19 01:11:57 +0600

    Complete

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,3 @@ use App\Http\Controllers\ProfileController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/profile/{id}', [ProfileController::class, 'index']);
